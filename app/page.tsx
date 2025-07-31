b947d9edfbe277a76861c2d5269d27dd4d3ce428
GovardhaneNitin <nitingovardhane000@gmail.com>
2025-07-31 14:30:23 +0530

    Added Demo Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import { Button } from "./components/ui/button";
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Welcome to Converso</h1>
-      <Button>Get Started</Button>
+      <Button>Let's Get Started</Button>
     </div>
   );
 };
