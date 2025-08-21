a40e6c2712e28f7fa86daa8b7e8bf5e2b406304c
Nitin Govardhane <nitingovardhane000@gmail.com>
2025-08-21 18:37:57 +0530

    feat: add Clerk auth & subscription (auth + billing)

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/app/components/NavItems";
@@ -12,7 +13,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
