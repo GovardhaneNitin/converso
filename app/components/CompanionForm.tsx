08b3aad0abdac450c5be98788d4a8708adf69b65
Nitin Govardhane <nitingovardhane000@gmail.com>
2025-08-26 01:29:14 +0530

    feat(supabase): add Supabase client and companion creation action

--- app/components/CompanionForm.tsx
+++ app/components/CompanionForm.tsx
@@ -23,6 +23,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "@/components/ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required." }),
@@ -48,8 +50,15 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: FormData) => {
-    console.log(values);
+  const onSubmit = async (values: FormData) => {
+    const companion = await createCompanion(values);
+
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create companion");
+      redirect("/");
+    }
   };
 
   return (
